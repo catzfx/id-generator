f0bf11ca3734d98f7361d8971e70a073e5c990d8
catzfx <iustinandi@gmail.com>
2023-04-14 01:38:40 +0300

    add documentation for checksum function

--- mrz.py
+++ mrz.py
@@ -1,4 +1,8 @@
 def checksum(l: list) -> int:
+    '''
+computes checksum for mrz code\n
+list items *must* be of str type
+    '''
     dictionary = {'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15, 'G': 16, 'H': 17, 'I': 18, 'J': 19, 'K': 20, 'L': 21, 'M': 22, 'N': 23, 'O': 24, 'P': 25, 'Q': 26, 'R': 27, 'S': 28, 'T': 29, 'U': 30, 'V': 31, 'W': 32, 'X': 33, 'Y': 34, 'Z': 35}
     weight = [7, 3, 1]
     sum = 0
